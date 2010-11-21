a67ff7cba14b116fb665091f59d9931231683f48
eagleea <eja2124@columbia.edu>
2010-11-21 17:12:58 -0500

    added stuff to test file, character, location decs

--- declaration.ml
+++ declaration.ml
@@ -26,9 +26,29 @@ let rec attrlist_to_java var attrlist = match attrlist with
 
 let itemdec_to_java str attrlist = ["//itemdec"; "Item " ^ str ^ " = new Item();"; "types.put(\"" ^ str ^ "\", Type.ITEM);"] @ (attrlist_to_java str attrlist)
 
-let charadec_to_java str attrlist itemlist = ["//charadec"]
 
-let locdec_to_java str attrlist itemlist charlist = ["//locdec"]
+
+let item_to_java (var:string) (item:membervar) : string list = match item with
+    Varref(str) -> [var ^".addItem(\"" ^str^ "\");"]
+	|Primember(pridec) ->["//This was bad. Not a reference to a complex variable"]
+
+let rec itemlist_to_java var itemlist = match itemlist with 
+    [] -> []
+    | hd::tl -> (item_to_java var hd) @ (itemlist_to_java var tl)
+
+let charadec_to_java str attrlist itemlist = ["//charadec"; "Character " ^ str ^ " = new Character();"; "types.put(\"" ^ str ^ "\", Type.CHARACTER);"]@ (attrlist_to_java str attrlist) @ (itemlist_to_java str itemlist)
+
+
+
+let character_to_java (var:string) (character:membervar) : string list = match character with
+    Varref(str) -> [var ^".showCharacter(\"" ^str^ "\");"]
+	|Primember(pridec) ->["//This was bad. Not a reference to a complex variable"]
+
+let rec characterlist_to_java var characterlist = match characterlist with 
+    [] -> []
+    | hd::tl -> (character_to_java var hd) @ (characterlist_to_java var tl)
+
+let locdec_to_java str attrlist itemlist charlist = ["//locdec"; "Location " ^ str ^ " = new Location();"; "types.put(\"" ^ str ^ "\", Type.LOCATION);"]@ (attrlist_to_java str attrlist) @ (itemlist_to_java str itemlist) @ (characterlist_to_java str charlist) 
 
 let intstrdec_to_java pridec = match pridec with
    Strdec(str) -> ["//strdec"; "String " ^ str ^ ";"]
