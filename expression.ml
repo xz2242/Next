0abc2cc49f20ceeb868672143eea042d1dd69e10
Danny Park <dsp2120@columbia.edu>
2010-11-23 10:22:39 -0500

    Edited expression.ml function signatures

--- expression.ml
+++ expression.ml
@@ -3,6 +3,7 @@ open Ast
 module type EXPRESSION = 
    sig 
     val expr_to_java : expr -> string
+	val expr_to_java_boolean : expr -> string list * string (* string list contains statements that need to happen before the string condition is checked *)
    end
 
 module Expression : EXPRESSION = struct
@@ -19,11 +20,11 @@ let rec expr_to_java exp = match exp with
                         else if op == Gt then (expr_to_java exp1) ^ " > " ^ (expr_to_java exp2)
                         else if op == Neq then (expr_to_java exp1) ^ " != " ^ (expr_to_java exp2)
                         else "WHAT"
-   | Asn (str, exp) -> str ^ " = " ^ (expr_to_java exp)
+   | Asn (type, str, exp) -> str ^ " = " ^ (expr_to_java exp)
    | Lit (i) -> string_of_int i
    | LitS (str) -> "\"" ^ str ^ "\""
-   | Exists (str1, str2) -> "characters.containsKey(\"" ^ str1 ^ "\") || locations.containsKey(\"" ^ str2 ^ "\")"
-   | Has (str1, str2) -> "//has WHAT IS THIS"
+   | Exists (type, str1, str2) -> "//TODO: match type and perform correct lookup characters.containsKey(\"" ^ str1 ^ "\") || locations.containsKey(\"" ^ str2 ^ "\")"
+   | Has (tup1, tup2) -> "//look up character or location and then look at attribute map of that object  //TODO: match type and look up in corresponding map"
    | Neg (exp) -> "-" ^ (expr_to_java exp)
    | Not (exp) -> "!" ^ (expr_to_java exp)
    | Var (str) -> str
