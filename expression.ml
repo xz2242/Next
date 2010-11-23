431deef4d420045bae8b71785bb34047eba28770
mulinski <mulinski@gmail.com>
2010-11-23 10:55:48 -0500

    version of expression.ml that compiles v Please enter the commit message for your changes. Lines starting

--- expression.ml
+++ expression.ml
@@ -3,7 +3,7 @@ open Ast
 module type EXPRESSION = 
    sig 
     val expr_to_java : expr -> string
-	val expr_to_java_boolean : expr -> string list * string (* string list contains statements that need to happen before the string condition is checked *)
+    val expr_to_java_boolean : expr -> string list * string (* string list contains statements that need to happen before the string condition is checked *)
    end
 
 module Expression : EXPRESSION = struct
@@ -20,13 +20,15 @@ let rec expr_to_java exp = match exp with
                         else if op == Gt then (expr_to_java exp1) ^ " > " ^ (expr_to_java exp2)
                         else if op == Neq then (expr_to_java exp1) ^ " != " ^ (expr_to_java exp2)
                         else "WHAT"
-   | Asn (type, str, exp) -> str ^ " = " ^ (expr_to_java exp)
+   | Asn (str, exp) -> str ^ " = " ^ (expr_to_java exp)
    | Lit (i) -> string_of_int i
    | LitS (str) -> "\"" ^ str ^ "\""
-   | Exists (type, str1, str2) -> "//TODO: match type and perform correct lookup characters.containsKey(\"" ^ str1 ^ "\") || locations.containsKey(\"" ^ str2 ^ "\")"
+   | Exists (str1, str2) -> "//TODO: match type and perform correct lookup characters.containsKey(\"" ^ str1 ^ "\") || locations.containsKey(\"" ^ str2 ^ "\")"
    | Has (tup1, tup2) -> "//look up character or location and then look at attribute map of that object  //TODO: match type and look up in corresponding map"
    | Neg (exp) -> "-" ^ (expr_to_java exp)
    | Not (exp) -> "!" ^ (expr_to_java exp)
    | Var (str) -> str
+
+let rec expr_to_java_boolean exp = ([], "true")
 
 end
