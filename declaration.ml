81c9372ef52fbef73810b2ff887be87b9f351b5c
Danny Park <dsp2120@columbia.edu>
2010-12-01 13:05:32 -0500

    updated declerations.ml to use check.ml

--- declaration.ml
+++ declaration.ml
@@ -1,12 +1,12 @@
 open Ast
 open Expression
-
+open Check
 
 module type DECLARATION = sig
-   val intstrdec_to_java : pridec -> string list
-   val itemdec_to_java : string -> membervarlist -> string list
-   val charadec_to_java : string -> membervarlist -> membervarlist -> string list
-   val locdec_to_java : string -> membervarlist -> membervarlist -> membervarlist -> string list
+   val intstrdec_to_java : pridec -> VarMap.t -> string list
+   val itemdec_to_java : string -> VarMap.t -> membervarlist -> string list
+   val charadec_to_java : string -> VarMap.t -> membervarlist -> membervarlist -> string list
+   val locdec_to_java : string -> VarMap.t -> membervarlist -> membervarlist -> membervarlist -> string list
 end
 
 
@@ -24,9 +24,8 @@ let rec attrlist_to_java var attrlist = match attrlist with
     [] -> []
     | hd::tl -> (attr_to_java var hd) @ (attrlist_to_java var tl)
 
-let itemdec_to_java str attrlist = ["//itemdec"; "Item " ^ str ^ " = new Item();"; "types.put(\"" ^ str ^ "\", Type.ITEM);"] @ (attrlist_to_java str attrlist)
-
-
+let itemdec_to_java str attrlist tmap = let t = check_id tmap str in 
+                                        ["//itemdec"; t ^ " " ^ str ^ " = new Item();"; "types.put(\"" ^ str ^ "\", Type.ITEM);"] @ (attrlist_to_java str attrlist)
 
 let item_to_java (var:string) (item:membervar) : string list = match item with
     Varref(str) -> [var ^".addItem(\"" ^str^ "\");"]
@@ -36,7 +35,8 @@ let rec itemlist_to_java var itemlist = match itemlist with
     [] -> []
     | hd::tl -> (item_to_java var hd) @ (itemlist_to_java var tl)
 
-let charadec_to_java str attrlist itemlist = ["//charadec"; "Character " ^ str ^ " = new Character();"; "types.put(\"" ^ str ^ "\", Type.CHARACTER);"]@ (attrlist_to_java str attrlist) @ (itemlist_to_java str itemlist)
+let charadec_to_java str attrlist itemlist tmap = let t = check_id tmap str in 
+                                                  ["//charadec"; t ^ " " ^ str ^ " = new Character();"; "types.put(\"" ^ str ^ "\", Type.CHARACTER);"]@ (attrlist_to_java str attrlist) @ (itemlist_to_java str itemlist)
 
 
 
@@ -48,12 +48,13 @@ let rec characterlist_to_java var characterlist = match characterlist with
     [] -> []
     | hd::tl -> (character_to_java var hd) @ (characterlist_to_java var tl)
 
-let locdec_to_java str attrlist itemlist charlist = ["//locdec"; "Location " ^ str ^ " = new Location();"; "types.put(\"" ^ str ^ "\", Type.LOCATION);"]@ (attrlist_to_java str attrlist) @ (itemlist_to_java str itemlist) @ (characterlist_to_java str charlist) 
+let locdec_to_java str attrlist itemlist charlist tmap = let t = check_id tmap str in 
+                                                        ["//locdec"; t ^ " " ^ str ^ " = new Location();"; "types.put(\"" ^ str ^ "\", Type.LOCATION);"]@ (attrlist_to_java str attrlist) @ (itemlist_to_java str itemlist) @ (characterlist_to_java str charlist) 
 
-let intstrdec_to_java pridec = match pridec with
-   Strdec(str) -> ["//strdec"; "String " ^ str ^ ";"]
-   | Intdec(str) -> ["//intdec"; "int " ^ str ^ ";"]
-   | Strdecinit(str, expr) -> ["//strdecinit"; "String " ^ str ^ " " ^ (Expression.expr_to_java expr) ^ ";"]
-   | Intdecinit(str, expr) -> ["//intdecinit"; "int " ^ str ^ " " ^ (Expression.expr_to_java expr) ^ ";"]
+let intstrdec_to_java pridec tmap = match pridec with
+   Strdec(str) -> let t = check_id tmap str in ["//strdec"; t ^ " " ^ str ^ ";"]
+   | Intdec(str) -> let t = check_id tmap str in ["//intdec"; t ^ " " ^ str ^ ";"]
+   | Strdecinit(str, expr) -> let t = check_id tmap str in ["//strdecinit"; t ^ " " ^ str ^ " " ^ (Expression.expr_to_java expr) ^ ";"]
+   | Intdecinit(str, expr) -> let t = check_id tmap str in ["//intdecinit"; t^ " " ^ str ^ " " ^ (Expression.expr_to_java expr) ^ ";"]
 
 end
--- expression.ml
+++ expression.ml
@@ -1,15 +1,16 @@
 open Ast
+open Check
 
 module type EXPRESSION = 
    sig 
-    val expr_to_java : expr -> string
+    val expr_to_java : expr -> VarMap.t -> string
     val expr_to_java_boolean : expr -> string list * string (* string list contains statements that need to happen before the string condition is checked *)
    end
 
 module Expression : EXPRESSION = struct
 
 
-let rec expr_to_java exp = match exp with
+let rec expr_to_java exp tmap = match exp with
    Binop (exp1, op, exp2) -> if op == Add then (expr_to_java exp1) ^ " + " ^ (expr_to_java exp2)
                         else if op == Sub then (expr_to_java exp1) ^ " - " ^ (expr_to_java exp2)
                         else if op == Mul then (expr_to_java exp1) ^ " * " ^ (expr_to_java exp2)
@@ -21,11 +22,13 @@ let rec expr_to_java exp = match exp with
                         else if op == Gt then (expr_to_java exp1) ^ " > " ^ (expr_to_java exp2)
                         else if op == Neq then (expr_to_java exp1) ^ " != " ^ (expr_to_java exp2)
                         else "WHAT"
-   | Asn (id, exp) -> "//assignment"
+   | Asn (id, exp) -> let t = check_id tmap id in 
+						print_next_type t ^ " " ^ id ^ " = " ^ (expr_to_java exp)
    | Lit (i) -> string_of_int i
    | LitS (str) -> "\"" ^ str ^ "\""
    | Exists (str1, str2) -> str1 ^ ".containsKey(\"" ^ str2 ^ "\")"
-   | Ident (id) -> "//look up character or location and then look at attribute map of that object  //TODO: match type and look up in corresponding map"
+   | Ident (id) -> let t = check_id tmap id in
+					
    | Neg (exp) -> "-" ^ (expr_to_java exp)
    | Not (exp) -> "!" ^ (expr_to_java exp)
    (*| Var (str) -> str*)
@@ -49,7 +52,7 @@ let rec expr_to_java_boolean exp = match exp with
 	   | Exists (str1, str2) -> ([], (expr_to_java exp))
 	   | Ident (id) -> ([], "true")
 	   | Neg (exp) -> ([], "-" ^ (expr_to_java exp) ^ " != 0 ")
-	   | Not (exp) -> ([], "true")
+	   | Not (exp) -> ([], (expr_to_java exp))
 	   (*| Var (str) -> ([], str ^ " != 0 ")*)
 
 
