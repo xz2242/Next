5cbc83129336d42b3654388da5dfaf1bdeaf2eb4
Danny Park <dsp2120@columbia.edu>
2010-12-01 14:53:53 -0500

    Fixed signatures in declaration.ml expression.ml

--- declaration.ml
+++ declaration.ml
@@ -3,29 +3,28 @@ open Expression
 open Check
 
 module type DECLARATION = sig
-   val intstrdec_to_java : pridec -> VarMap.t -> string list
-   val itemdec_to_java : string -> VarMap.t -> membervarlist -> string list
-   val charadec_to_java : string -> VarMap.t -> membervarlist -> membervarlist -> string list
-   val locdec_to_java : string -> VarMap.t -> membervarlist -> membervarlist -> membervarlist -> string list
+   val intstrdec_to_java : pridec -> ('a VarMap.t VarMap.t) -> string list
+   val itemdec_to_java : string -> membervarlist -> ('a VarMap.t VarMap.t) -> string list
+   val charadec_to_java : string -> membervarlist -> membervarlist -> ('a VarMap.t VarMap.t) -> string list
+   val locdec_to_java : string -> membervarlist -> membervarlist -> membervarlist -> ('a VarMap.t VarMap.t)  -> string list
 end
 
 
 module Declaration : DECLARATION = struct
 
-let attr_to_java (var:string) (attr:membervar) : string list = match attr with
+let attr_to_java tmap (var:string) (attr:membervar) : string list = match attr with
    Primember(pridec) -> (match pridec with 
        Strdec (str) -> [var ^ ".addStrAttr(\"" ^ str ^ "\", \"\");"; "types.put(\"" ^ str ^ "\", Type.STRING);"]
        | Intdec (str) -> [var ^ ".addIntAttr(\"" ^ str ^ "\", 0);"; "types.put(\"" ^ str ^ "\", Type.INT);"]
-       | Strdecinit (str, expr) -> [var ^ ".addStrAttr(\"" ^ str ^ "\"," ^ (Expression.expr_to_java expr) ^ ");"; "types.put(\"" ^ str ^ "\", Type.STRING);"]
-       | Intdecinit (str, expr) -> [ var ^ ".addIntAttr(\"" ^ str ^ "\", " ^ (Expression.expr_to_java expr) ^ ");"; "types.put(\"" ^ str ^ "\", Type.INT);"])
+       | Strdecinit (str, expr) -> [var ^ ".addStrAttr(\"" ^ str ^ "\"," ^ (Expression.expr_to_java expr tmap) ^ ");"; "types.put(\"" ^ str ^ "\", Type.STRING);"]
+       | Intdecinit (str, expr) -> [ var ^ ".addIntAttr(\"" ^ str ^ "\", " ^ (Expression.expr_to_java expr tmap) ^ ");"; "types.put(\"" ^ str ^ "\", Type.INT);"])
    | Varref(str) -> ["//OOPS THIS WAS BAD!"]
 
-let rec attrlist_to_java var attrlist = match attrlist with 
+let rec attrlist_to_java var attrlist tmap = match attrlist with 
     [] -> []
-    | hd::tl -> (attr_to_java var hd) @ (attrlist_to_java var tl)
+    | hd::tl -> (attr_to_java tmap var hd) @ (attrlist_to_java var tl tmap)
 
-let itemdec_to_java str attrlist tmap = let t = check_id tmap str in 
-                                        ["//itemdec"; t ^ " " ^ str ^ " = new Item();"; "types.put(\"" ^ str ^ "\", Type.ITEM);"] @ (attrlist_to_java str attrlist)
+let itemdec_to_java str attrlist tmap = ["//itemdec"; "Item " ^ str ^ " = new Item();"; "types.put(\"" ^ str ^ "\", Type.ITEM);"] @ (attrlist_to_java str attrlist tmap)
 
 let item_to_java (var:string) (item:membervar) : string list = match item with
     Varref(str) -> [var ^".addItem(\"" ^str^ "\");"]
@@ -35,10 +34,7 @@ let rec itemlist_to_java var itemlist = match itemlist with
     [] -> []
     | hd::tl -> (item_to_java var hd) @ (itemlist_to_java var tl)
 
-let charadec_to_java str attrlist itemlist tmap = let t = check_id tmap str in 
-                                                  ["//charadec"; t ^ " " ^ str ^ " = new Character();"; "types.put(\"" ^ str ^ "\", Type.CHARACTER);"]@ (attrlist_to_java str attrlist) @ (itemlist_to_java str itemlist)
-
-
+let charadec_to_java str attrlist itemlist tmap = ["//charadec"; "Character " ^ str ^ " = new Character();"; "types.put(\"" ^ str ^ "\", Type.CHARACTER);"]@ (attrlist_to_java str attrlist tmap) @ (itemlist_to_java str itemlist)
 
 let character_to_java (var:string) (character:membervar) : string list = match character with
     Varref(str) -> [var ^".showCharacter(\"" ^str^ "\");"]
@@ -48,13 +44,12 @@ let rec characterlist_to_java var characterlist = match characterlist with
     [] -> []
     | hd::tl -> (character_to_java var hd) @ (characterlist_to_java var tl)
 
-let locdec_to_java str attrlist itemlist charlist tmap = let t = check_id tmap str in 
-                                                        ["//locdec"; t ^ " " ^ str ^ " = new Location();"; "types.put(\"" ^ str ^ "\", Type.LOCATION);"]@ (attrlist_to_java str attrlist) @ (itemlist_to_java str itemlist) @ (characterlist_to_java str charlist) 
+let locdec_to_java str attrlist itemlist charlist tmap = ["//locdec"; "Location " ^ str ^ " = new Location();"; "types.put(\"" ^ str ^ "\", Type.LOCATION);"]@ (attrlist_to_java str attrlist tmap) @ (itemlist_to_java str itemlist) @ (characterlist_to_java str charlist) 
 
 let intstrdec_to_java pridec tmap = match pridec with
-   Strdec(str) -> let t = check_id tmap str in ["//strdec"; t ^ " " ^ str ^ ";"]
-   | Intdec(str) -> let t = check_id tmap str in ["//intdec"; t ^ " " ^ str ^ ";"]
-   | Strdecinit(str, expr) -> let t = check_id tmap str in ["//strdecinit"; t ^ " " ^ str ^ " " ^ (Expression.expr_to_java expr) ^ ";"]
-   | Intdecinit(str, expr) -> let t = check_id tmap str in ["//intdecinit"; t^ " " ^ str ^ " " ^ (Expression.expr_to_java expr) ^ ";"]
+   Strdec(str) -> ["//strdec"; "String " ^ str ^ ";"]
+   | Intdec(str) -> ["//intdec"; "int " ^ str ^ ";"]
+   | Strdecinit(str, expr) -> ["//strdecinit"; "String " ^ str ^ " " ^ (Expression.expr_to_java expr tmap) ^ ";"]
+   | Intdecinit(str, expr) -> ["//intdecinit"; "int " ^ str ^ " " ^ (Expression.expr_to_java expr tmap) ^ ";"]
 
 end
--- expression.ml
+++ expression.ml
@@ -1,58 +1,71 @@
 open Ast
+open Checktype
 open Check
+
 
 module type EXPRESSION = 
    sig 
-    val expr_to_java : expr -> VarMap.t -> string
-    val expr_to_java_boolean : expr -> string list * string (* string list contains statements that need to happen before the string condition is checked *)
+    val expr_to_java : expr -> ('a VarMap.t VarMap.t) -> string
+    val expr_to_java_boolean : expr -> ('a VarMap.t VarMap.t) -> string list * string (* string list contains statements that need to happen before the string condition is checked *)
+    val next_type_to_string : Checktype.t -> string
    end
 
 module Expression : EXPRESSION = struct
 
+let next_type_to_string = function
+  	Integer -> "int"
+    | String -> "string"
+    | Character -> "character"
+    | Item -> "item"
+    | Location -> "location"
+    | Action -> "action"
+    | Key -> "key"
 
 let rec expr_to_java exp tmap = match exp with
-   Binop (exp1, op, exp2) -> if op == Add then (expr_to_java exp1) ^ " + " ^ (expr_to_java exp2)
-                        else if op == Sub then (expr_to_java exp1) ^ " - " ^ (expr_to_java exp2)
-                        else if op == Mul then (expr_to_java exp1) ^ " * " ^ (expr_to_java exp2)
-                        else if op == Div then (expr_to_java exp1) ^ " / " ^ (expr_to_java exp2)
+   Binop (exp1, op, exp2) -> if op == Add then "(" ^ (expr_to_java exp1 tmap) ^ ") + (" ^ (expr_to_java exp2 tmap) ^ ")"
+                        else if op == Sub then "(" ^ (expr_to_java exp1 tmap) ^ ") - (" ^ (expr_to_java exp2 tmap) ^ ")"
+                        else if op == Mul then "(" ^ (expr_to_java exp1 tmap) ^ ") * (" ^ (expr_to_java exp2 tmap) ^ ")"
+                        else if op == Div then "(" ^ (expr_to_java exp1 tmap) ^ ") / (" ^ (expr_to_java exp2 tmap) ^ ")"
                         (*else if op == Or then (expr_to_java exp1) ^ " || " ^ (expr_to_java exp2)
                         else if op == And then (expr_to_java exp1) ^ " && " ^ (expr_to_java exp2)*)
-                        else if op == Eq then (expr_to_java exp1) ^ " == " ^ (expr_to_java exp2) (* what about string equality? *)
-                        else if op == Lt then (expr_to_java exp1) ^ " < " ^ (expr_to_java exp2)
-                        else if op == Gt then (expr_to_java exp1) ^ " > " ^ (expr_to_java exp2)
-                        else if op == Neq then (expr_to_java exp1) ^ " != " ^ (expr_to_java exp2)
+                        else if op == Eq then "(" ^ (expr_to_java exp1 tmap) ^ ") == (" ^ (expr_to_java exp2 tmap) ^ ")" (* what about string equality? *)
+                        else if op == Lt then "(" ^ (expr_to_java exp1 tmap) ^ ") < (" ^ (expr_to_java exp2 tmap) ^ ")"
+                        else if op == Gt then "(" ^ (expr_to_java exp1 tmap) ^ ") > (" ^ (expr_to_java exp2 tmap) ^ ")"
+                        else if op == Neq then "(" ^ (expr_to_java exp1 tmap) ^ ") != (" ^ (expr_to_java exp2 tmap) ^ ")"
                         else "WHAT"
    | Asn (id, exp) -> let t = check_id tmap id in 
-						print_next_type t ^ " " ^ id ^ " = " ^ (expr_to_java exp)
-   | Lit (i) -> string_of_int i
-   | LitS (str) -> "\"" ^ str ^ "\""
+                        (match id with
+                        Var(str) -> (next_type_to_string t) ^ " " ^ str ^ " = " ^ (expr_to_java exp tmap)
+                      | Has(str1, str2) -> (next_type_to_string t) ^ " " ^ str2 ^ " = " ^ (expr_to_java exp tmap))
+   | Lit (i) -> "(" ^ (string_of_int i) ^ ")"
+   | LitS (str) -> "(\"" ^ str ^ "\")"
    | Exists (str1, str2) -> str1 ^ ".containsKey(\"" ^ str2 ^ "\")"
-   | Ident (id) -> let t = check_id tmap id in
+   | Ident (id) -> "let t = check_id tmap id in"
 					
-   | Neg (exp) -> "-" ^ (expr_to_java exp)
-   | Not (exp) -> "!" ^ (expr_to_java exp)
+   | Neg (exp) -> "(-" ^ (expr_to_java exp tmap) ^ ")"
+   | Not (exp) -> "(!" ^ (expr_to_java exp tmap) ^ ")"
    (*| Var (str) -> str*)
 
 
-let rec expr_to_java_boolean exp = match exp with
-	Binop (exp1, op, exp2) -> if op == Add then ([], (expr_to_java exp1) ^ " + " ^ (expr_to_java exp2) ^ " != 0" )
-	                        else if op == Sub then ([], (expr_to_java exp1) ^ " - " ^ (expr_to_java exp2) ^ " != 0")
-	                        else if op == Mul then ([], (expr_to_java exp1) ^ " * " ^ (expr_to_java exp2) ^ " != 0")
-	                        else if op == Div then ([], (expr_to_java exp1) ^ " / " ^ (expr_to_java exp2) ^ " != 0")
-	                        else if op == Eq then ([], (expr_to_java exp1) ^ " == " ^ (expr_to_java exp2)) (* what about string equality? *)
-	                        else if op == Lt then ([], (expr_to_java exp1) ^ " < " ^ (expr_to_java exp2))
-		                    else if op == Gt then ([], (expr_to_java exp1) ^ " > " ^ (expr_to_java exp2))
-		                    else if op == Neq then ([], (expr_to_java exp1) ^ " != " ^ (expr_to_java exp2))
-							else if op == Or then  (fst (expr_to_java_boolean exp1)@fst (expr_to_java_boolean exp2), (snd (expr_to_java_boolean exp1)) ^ " || " ^ (snd (expr_to_java_boolean exp2)))
-                        	else if op == And then (fst (expr_to_java_boolean exp1)@fst (expr_to_java_boolean exp2), (snd (expr_to_java_boolean exp1)) ^ " && " ^ (snd (expr_to_java_boolean exp2)))
+let rec expr_to_java_boolean exp tmap = match exp with
+	Binop (exp1, op, exp2) -> if op == Add then ([], (expr_to_java exp1 tmap) ^ " + " ^ (expr_to_java exp2 tmap) ^ " != 0" )
+	                        else if op == Sub then ([], (expr_to_java exp1 tmap) ^ " - " ^ (expr_to_java exp2 tmap) ^ " != 0")
+	                        else if op == Mul then ([], (expr_to_java exp1 tmap) ^ " * " ^ (expr_to_java exp2 tmap) ^ " != 0")
+	                        else if op == Div then ([], (expr_to_java exp1 tmap) ^ " / " ^ (expr_to_java exp2 tmap) ^ " != 0")
+	                        else if op == Eq then ([], (expr_to_java exp1 tmap) ^ " == " ^ (expr_to_java exp2 tmap)) (* what about string equality? *)
+	                        else if op == Lt then ([], (expr_to_java exp1 tmap) ^ " < " ^ (expr_to_java exp2 tmap))
+		                    else if op == Gt then ([], (expr_to_java exp1 tmap) ^ " > " ^ (expr_to_java exp2 tmap))
+		                    else if op == Neq then ([], (expr_to_java exp1 tmap) ^ " != " ^ (expr_to_java exp2 tmap))
+							else if op == Or then  (fst (expr_to_java_boolean exp1 tmap)@fst (expr_to_java_boolean exp2 tmap), (snd (expr_to_java_boolean exp1 tmap)) ^ " || " ^ (snd (expr_to_java_boolean exp2 tmap)))
+                        	else if op == And then (fst (expr_to_java_boolean exp1 tmap)@fst (expr_to_java_boolean exp2 tmap), (snd (expr_to_java_boolean exp1 tmap)) ^ " && " ^ (snd (expr_to_java_boolean exp2 tmap)))
 	                        else ([], "false")
 	   | Asn (id, exp) -> ([], "true")
 	   | Lit (i) -> ([], string_of_int i ^ " != 0")
 	   | LitS (str) -> ([], "true")
-	   | Exists (str1, str2) -> ([], (expr_to_java exp))
+	   | Exists (str1, str2) -> ([], (expr_to_java exp tmap))
 	   | Ident (id) -> ([], "true")
-	   | Neg (exp) -> ([], "-" ^ (expr_to_java exp) ^ " != 0 ")
-	   | Not (exp) -> ([], (expr_to_java exp))
+	   | Neg (exp) -> ([], (expr_to_java exp tmap) ^ ") != 0 ")
+	   | Not (exp) -> ([], (expr_to_java exp tmap))
 	   (*| Var (str) -> ([], str ^ " != 0 ")*)
 
 
