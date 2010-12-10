0cc9fa590913546381871596df7a3e05dea066ca
Danny Park <dsp2120@columbia.edu>
2010-12-10 13:31:47 -0500

    Clean script would not remove Next$Type.class so added a new line to do it

--- CleanTests.pl
+++ CleanTests.pl
@@ -3,6 +3,8 @@
 @testFileDirs = <Tests/*>;
 
 foreach $testFileDir(@testFileDirs) {
+    system("rm ". $testFileDir . "/Next" . '\$' . "Type.class");
+    
     @testFileDir = <$testFileDir/*>;
     foreach $testFile(@testFileDir) {
         if(!($testFile =~ /(\.next)$/i) && !($testFile =~ /reference/i)) {
