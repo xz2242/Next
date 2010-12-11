042caf7b5b93bfe674959b9f6c9f8a04d5d9d0c6
Danny Park <dsp2120@columbia.edu>
2010-12-10 19:41:48 -0500

    Added code to run scripts in FailTests; Added FailTests folder

---
+++ RunFailTests.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+
+use File::Compare;
+use FileHandle;
+
+my @failedTestFiles = ();
+
+@testFileDirs = <FailedTests/*>;
+
+foreach $testFileDir(@testFileDirs) {
+    @testFileDir = <$testFileDir/*>;
+    foreach $testFile(@testFileDir) {
+        if($testFile =~ /\.next$/i) {
+            #do the compilation
+            if(system("./next < " . $testFile . " > " . $testFileDir . "/Next.java") == 0) {
+                push(@failedTestFiles, $testFileDir);
+            }
+        }
+    }
+}
+
+
+#write to log file
+$fh = FileHandle -> new();
+if($fh -> open("> FailTests/SuccesfulTests.txt")) {
+    foreach $file(@failedTestFiles) {
+        $fh -> print($file . "\n");
+    }
+}
